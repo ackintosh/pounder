648299bc5ea7bc510efcea602c3507cb2f3eb4ad
Akihito Nakano <sora.akatsuki@gmail.com>
2014-03-15 15:07:03 +0900

    Add test

--- spec/pounder/server/command_spec.rb
+++ spec/pounder/server/command_spec.rb
@@ -16,4 +16,35 @@ describe Pounder::Server::Command do
       @server.cmd_STAT(maildir: @maildir, args: [])
     end
   end
+
+  describe "#cmd_QUIT" do
+    it "prints '+OK' and terminate the thread" do
+      output = double("TCPSocket")
+      output.should_receive(:print).with("+OK\r\n")
+      @server.instance_variable_set(:@output, output)
+      expect(@server).to receive(:terminate)
+      @server.send(:cmd_QUIT, maildir: @maildir, args: [])
+    end
+  end
+
+  shared_examples("no operation") do |cmd|
+    it "prints '+OK'" do
+      output = double("TCPSocket")
+      output.should_receive(:print).with("+OK\r\n")
+      @server.instance_variable_set(:@output, output)
+      @server.send(cmd, maildir: @maildir, args: [])
+    end
+  end
+
+  describe "#cmd_USER" do
+    it_behaves_like "no operation", :cmd_USER
+  end
+
+  describe "#cmd_PASS" do
+    it_behaves_like "no operation", :cmd_PASS
+  end
+
+  describe "#cmd_DELE" do
+    it_behaves_like "no operation", :cmd_DELE
+  end
 end
